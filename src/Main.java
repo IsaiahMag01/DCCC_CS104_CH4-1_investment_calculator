b39dcc0ae05cbc32c039ab680c68dd95b6dff86a
Isaiah Magana <Isaiah2Mag@gmail.com>
2023-11-10 21:58:57 -0500

    Implement the pseudocode
    Implement the initial pseudocode of the program

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,18 @@ public class Main {
      * @param main
      */
     public static void main(String[] args) {
+        final double RATE = 0.05;   //The rate is fixed and should never change in the program itself
+        final double TARGET = 0.05; //Terminator for the program
+        double balance = 10_000;
+        int year = 0;
+
+        while (balance < TARGET) {
+            year++;
+            double interest = balance * RATE;
+            balance = balance + interest;
+        }
+
+        System.out.println("Within " + year + " year(s) you will have a balance of " + balance + " dollars");
 
     }
 }
